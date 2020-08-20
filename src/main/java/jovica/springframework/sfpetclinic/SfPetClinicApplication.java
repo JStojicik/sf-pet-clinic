f7629038394cc388c217c5e218ef2257035fc995
jamvo <j.stojicik@gmail.com>
2020-08-20 16:41:08 +0200

    deleating unnececery line

--- src/main/java/jovica/springframework/sfpetclinic/SfPetClinicApplication.java
+++ src/main/java/jovica/springframework/sfpetclinic/SfPetClinicApplication.java
@@ -8,7 +8,6 @@ public class SfPetClinicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SfPetClinicApplication.class, args);
-        System.out.println("asd");
     }
 
 }
