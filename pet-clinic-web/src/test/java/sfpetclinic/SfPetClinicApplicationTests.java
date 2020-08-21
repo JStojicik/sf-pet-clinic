6e0e3f05de71ac653fd8aa5cda28e09cf14cee73
jamvo <j.stojicik@gmail.com>
2020-08-21 18:31:31 +0200

    Refactoring to multy module maven project. Closes #1

---
+++ pet-clinic-web/src/test/java/sfpetclinic/SfPetClinicApplicationTests.java
@@ -0,0 +1,17 @@
+package sfpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+//@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@SpringBootConfiguration
+class SfPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
