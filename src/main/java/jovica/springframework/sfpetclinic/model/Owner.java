586aef64dcb97dfa4c7fa7e0339ad497be955712
jamvo <j.stojicik@gmail.com>
2020-08-21 17:19:24 +0200

    Defining basic data model-adding POJOs. Closes #2.

---
+++ src/main/java/jovica/springframework/sfpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package jovica.springframework.sfpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/jovica/springframework/sfpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package jovica.springframework.sfpetclinic.model;
+
+public class Vet extends Person {
+}
