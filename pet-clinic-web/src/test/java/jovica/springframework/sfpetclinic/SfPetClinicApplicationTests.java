824b3faf76ae9ed3f451426842d687ecc6db30ca
jamvo <j.stojicik@gmail.com>
2020-08-21 19:23:07 +0200

    testing

--- pet-clinic-web/src/test/java/jovica/springframework/sfpetclinic/SfPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/jovica/springframework/sfpetclinic/SfPetClinicApplicationTests.java
@@ -1,15 +1,12 @@
-package sfpetclinic;
+package jovica.springframework.sfpetclinic;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 //@ExtendWith(SpringExtension.class)
 @SpringBootTest
-@SpringBootConfiguration
 class SfPetClinicApplicationTests {
-
     @Test
     void contextLoads() {
     }
