7e475b319fd2643b9db1edeb2e6a6c7210e263d7
jamvo <j.stojicik@gmail.com>
2020-08-22 17:05:08 +0200

    Impl Map based Services. closes #6

---
+++ pet-clinic-data/src/main/java/jovica/springframework/sfpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,37 @@
+package jovica.springframework.sfpetclinic.services.map;
+
+import jovica.springframework.sfpetclinic.model.Owner;
+import jovica.springframework.sfpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+
+}
---
+++ pet-clinic-data/src/main/java/jovica/springframework/sfpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,38 @@
+package jovica.springframework.sfpetclinic.services.map;
+
+
+import jovica.springframework.sfpetclinic.model.Vet;
+import jovica.springframework.sfpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+
+}
