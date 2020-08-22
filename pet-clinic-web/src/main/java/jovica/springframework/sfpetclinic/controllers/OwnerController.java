2071ac429ada1e6d160b4976cdad17dd527a7b26
jamvo <j.stojicik@gmail.com>
2020-08-22 18:05:08 +0200

    Adding owners page and controller. closes #13

--- pet-clinic-web/src/main/java/jovica/springframework/sfpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/jovica/springframework/sfpetclinic/controllers/OwnerController.java
@@ -2,11 +2,11 @@ package jovica.springframework.sfpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@RequestMapping("/owners")
 @Controller
-public class IndexController {
-    @RequestMapping({"", "/", "index", "index.html"})
-    public String index() {
-        return "index";
+public class OwnerController {
+    @RequestMapping({"","/", "/index", " /index.html"})
+    public String listOwners() {
+        return "owners/index";
     }
 }
