4e4115c89996d0e9158d992c348a36a057377cc7
jamvo <j.stojicik@gmail.com>
2020-08-24 17:26:54 +0200

    added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/jovica/springframework/sfpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package jovica.springframework.sfpetclinic.services;
+
+import jovica.springframework.sfpetclinic.model.PetType;
+
+public interface PetTypeService  extends CrudService<PetType,Long>{
+}
