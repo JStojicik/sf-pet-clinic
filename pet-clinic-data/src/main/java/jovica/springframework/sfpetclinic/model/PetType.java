805a9f8ee120afe2f2c691c051a18f91925253ed
jamvo <j.stojicik@gmail.com>
2020-08-25 17:43:53 +0200

    Updates Owner with JPA annotation. Closes #36

--- pet-clinic-data/src/main/java/jovica/springframework/sfpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/jovica/springframework/sfpetclinic/model/PetType.java
@@ -1,8 +1,17 @@
 package jovica.springframework.sfpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
+
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "last_name")
     public String getName() {
         return name;
     }
