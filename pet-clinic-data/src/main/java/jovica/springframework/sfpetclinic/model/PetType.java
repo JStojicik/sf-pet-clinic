2672a8cebbc25f443e1c8fd403a55a7e2421393d
jamvo <j.stojicik@gmail.com>
2020-08-25 20:40:22 +0200

    Added project Lombok to SpringPetClinic project. Closes #51

--- pet-clinic-data/src/main/java/jovica/springframework/sfpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/jovica/springframework/sfpetclinic/model/PetType.java
@@ -1,22 +1,19 @@
 package jovica.springframework.sfpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity{
 
     @Column(name = "name")
     private String name;
-
-    @Column(name = "last_name")
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
