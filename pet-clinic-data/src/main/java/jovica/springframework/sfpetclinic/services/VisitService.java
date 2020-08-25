cea093710aecbed9024127bcdc7c204d8262ab51
jamvo <j.stojicik@gmail.com>
2020-08-25 19:17:19 +0200

    Implemented Map impl for VisitService. Closes #46

---
+++ pet-clinic-data/src/main/java/jovica/springframework/sfpetclinic/services/VisitService.java
@@ -0,0 +1,8 @@
+package jovica.springframework.sfpetclinic.services;
+
+import jovica.springframework.sfpetclinic.model.Visit;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface VisitService extends CrudService<Visit, Long>{
+}
