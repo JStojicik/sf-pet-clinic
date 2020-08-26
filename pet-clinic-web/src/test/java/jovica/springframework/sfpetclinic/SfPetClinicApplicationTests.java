86498a60e51435c2be01e13744ea7cf022dc77a8
jamvo <j.stojicik@gmail.com>
2020-08-26 19:03:30 +0200

    Allready on JUnit 5 just updated some @Annotations. Closes #4

--- pet-clinic-web/src/test/java/jovica/springframework/sfpetclinic/SfPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/jovica/springframework/sfpetclinic/SfPetClinicApplicationTests.java
@@ -1,10 +1,13 @@
 package jovica.springframework.sfpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 //import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 //@ExtendWith(SpringExtension.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SfPetClinicApplicationTests {
     @Test
